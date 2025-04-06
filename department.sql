2efa96d7e80c8ce01bbe51f0c561eff41bdf8731
Insung1007 <dlstjd2686@naver.com>
2025-04-06 17:36:32 +0900

    Add files via upload

---
+++ department.sql
@@ -0,0 +1,10 @@
+/*
+-- Query: SELECT * FROM 한빛무역.`department.sql`
+LIMIT 0, 50000
+
+-- Date: 2025-04-06 17:20
+*/
+INSERT INTO `` (`DepartmentNumber`,`DepartmentName`) VALUES ('A1','영업부');
+INSERT INTO `` (`DepartmentNumber`,`DepartmentName`) VALUES ('A2','기획부');
+INSERT INTO `` (`DepartmentNumber`,`DepartmentName`) VALUES ('A3','개발부');
+INSERT INTO `` (`DepartmentNumber`,`DepartmentName`) VALUES ('A4','홍보부');
---
+++ shoppingtype.sql
@@ -0,0 +1,12 @@
+/*
+-- Query: SELECT * FROM 한빛무역.`shoppingtype.sql`
+LIMIT 0, 50000
+
+-- Date: 2025-04-06 17:21
+*/
+INSERT INTO `` (`ShoppingNum`,`name`) VALUES (1,'네이버쇼핑몰');
+INSERT INTO `` (`ShoppingNum`,`name`) VALUES (2,'쿠팡');
+INSERT INTO `` (`ShoppingNum`,`name`) VALUES (3,'알리익스프레스');
+INSERT INTO `` (`ShoppingNum`,`name`) VALUES (4,'마켓컬리');
+INSERT INTO `` (`ShoppingNum`,`name`) VALUES (10,'테무');
+INSERT INTO `` (`ShoppingNum`,`name`) VALUES (11,'중고나라');
